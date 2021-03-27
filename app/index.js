131fada6e8859dbc4a1b9a0c9866459a8f6c04e0
Xin00163 <wangxin00163@yahoo.co.uk>
2021-03-27 15:59:45 +0000

    feat: nav component and setup lint

--- app/index.js
+++ app/index.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { ThemeProvider } from './contexts/theme'
+import Nav from "./components/Nav";
+import {
+  BrowserRouter as Router} from "react-router-dom";
 
 class App extends React.Component {
+  state = {
+    theme: 'light',
+    toggleTheme: () => {
+      this.setState(({theme}) => ({
+        theme: theme === 'light'? 'dark' : 'light'
+      }))
+    }
+  }
   render() {
-    return <div>Hello Hackers</div>
+    return (
+      <Router>
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+          </div>
+        </div>
+      </ThemeProvider>
+      </Router>
+    );
   }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById("app"));
